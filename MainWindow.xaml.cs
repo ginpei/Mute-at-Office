9d2317bf7aed110a38ebed1c728be20b061cc1b6
Ginpei <ginpei@ginpei.dev>
2025-11-15 18:08:08 -0800

    task tray

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Mute_at_Office.Pages.Dashboard;
 using Mute_at_Office.Pages.Settings;
+using CommunityToolkit.Mvvm.Input;
+using H.NotifyIcon;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -25,14 +27,41 @@ namespace Mute_at_Office
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private bool _isExitRequested;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.Closed += OnWindowClosed;
 
             AppWindow.Resize(new Windows.Graphics.SizeInt32 { Width = 1200, Height = 1200 });
 
             ContentFrame.Navigate(typeof(DashboardPage));
             MainNavigationView.SelectedItem = MainNavigationView.MenuItems[0];
+        }
+
+        private void OnWindowClosed(object sender, WindowEventArgs args)
+        {
+            if (!_isExitRequested)
+            {
+                args.Handled = true;
+                this.Hide();
+            }
+        }
+
+        [RelayCommand]
+        public void CloseWindow()
+        {
+            _isExitRequested = true;
+            Application.Current.Exit();
+        }
+
+        [RelayCommand]
+        public void ShowWindow()
+        {
+            this.Show();
+            this.Activate();
         }
 
         private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
