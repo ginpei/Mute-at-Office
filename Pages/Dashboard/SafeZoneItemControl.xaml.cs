7c08008db54f5fd62e16d28ee57c3fd5e4bad4db
Ginpei <ginpei@ginpei.dev>
2025-12-25 14:10:50 -0800

    extract to a user control

---
+++ Pages/Dashboard/SafeZoneItemControl.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace Mute_at_Office.Pages.Dashboard;
+
+public sealed partial class SafeZoneItemControl : UserControl
+{
+    public SafeZoneItemControl()
+    {
+        this.InitializeComponent();
+    }
+}
